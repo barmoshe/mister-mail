dce2736d147c994fb51f39191db5ce66b6879466
barmoshe <1barmoshe1@gmail.com>
2024-03-05 15:47:46 +0200

    added filtering option

--- src/cmps/EmailFilter.jsx
+++ src/cmps/EmailFilter.jsx
@@ -1,46 +1,49 @@
 import React, { useEffect, useState } from "react";
 
-// <EmailFilter> - allow the user to filter the emails by text & isRead
+// <EmailFilter> - allow the user to filter the emails by  subject&body , read/unread,star/unstar
 
 export function EmailFilter({ filterBy, onSetFilter }) {
   const [filterByToEdit, setFilterByToEdit] = useState(filterBy);
+  console.log(filterByToEdit, "1filterByToEdit");
+  console.log(filterBy, "1filterBy");
 
   useEffect(() => {
     onSetFilter(filterByToEdit);
   }, [filterByToEdit]);
 
-  function onSubmitFilter(ev) {
-    ev.preventDefault();
-    onSetFilter(filterByToEdit);
-  }
-
   function handleChange(ev) {
-    let { value, name: field } = ev.target;
-    setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }));
+    let { value, name: field, type } = ev.target;
+    value = type === "number" ? +value : value;
+    setFilterByToEdit({ ...filterByToEdit, [field]: value });
   }
 
+  //filter form attributes :  subject&body- text input , read/unread- select input,star /unstar- select input
   return (
-    <form className="email-filter" onSubmit={onSubmitFilter}>
-      <label>
-        Search
-        <input
-          type="text"
-          placeholder="Search by text"
-          value={filterByToEdit.txt}
-          name="txt"
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={filterByToEdit.isRead}
-          name="isRead"
-          onChange={handleChange}
-        />
-        Read
-      </label>
-      <button>Filter</button>
+    <form className="email-filter">
+      <input
+        type="text"
+        name="text"
+        value={filterByToEdit.text}
+        onChange={handleChange}
+      />
+      <select
+        name="isRead"
+        value={filterByToEdit.isRead}
+        onChange={handleChange}
+      >
+        <option value="all">All</option>
+        <option value="true">Read</option>
+        <option value="false">Unread</option>
+      </select>
+      <select
+        name="isStarred"
+        value={filterByToEdit.isStarred}
+        onChange={handleChange}
+      >
+        <option value="all">All</option>
+        <option value="true">Starred</option>
+        <option value="false">Unstarred</option>
+      </select>
     </form>
   );
 }
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -7,25 +7,20 @@ export const emailService = {
   remove,
   save,
   createEmail,
-  getEmptyEmail,
+
   getDefaultFilter,
 };
-const STORAGE_KEY = "emails";
+const STORAGE_KEY = "emails_db";
 _createEmails();
 
-function query(filterBy) {
-  const emails = storageService.query(STORAGE_KEY);
+async function query(filterBy) {
+  let emails = await storageService.query(STORAGE_KEY);
   if (filterBy) {
-    var { subject, isRead, isStarred } = filterBy;
-    emails = emails.filter(
-      (email) =>
-        email.subject.toLowerCase().includes(subject.toLowerCase()) &&
-        email.isRead === isRead &&
-        email.isStarred === isStarred
-    );
+    emails = _filter(emails, filterBy);
   }
   return emails;
 }
+
 function getById(id) {
   return storageService.get(STORAGE_KEY, id);
 }
@@ -54,12 +49,22 @@ function createEmail(subject = "", body = "", from = "", to = "") {
   };
 }
 
-function getEmptyEmail() {
+// function getEmptyEmail() {
+//   return {
+//     subject: "",
+//     body: "",
+//     from: "",
+//     to: "",
+//     isRead: false,
+//     isStarred: false,
+//     sentAt: 0,
+//   };
+// }
+function getDefaultFilter() {
   return {
-    subject: "",
-    body: "",
-    from: "",
-    to: "",
+    text: "",
+    isRead: "all",
+    isStarred: "all",
   };
 }
 
@@ -69,41 +74,108 @@ function _createEmails() {
     emails = [
       {
         id: "e101",
-        subject: "Miss you!",
-        body: "Would love to catch up sometimes",
+        subject: "Reconnect: Let's Catch Up Over Coffee",
+        body: "Hey there! It's been a while since we last spoke. I'd love to hear how you've been. How about we grab a coffee sometime soon and catch up?",
         isRead: false,
-        isStarred: false,
-        sentAt: 1551133930594,
+        isStarred: true,
+        sentAt: 1646361600000, // March 3, 2022
         from: "user@appsus.com",
         to: "autoMail2@gmail.com",
       },
       {
         id: "e102",
-        subject: "Hello!",
-        body: "I am here",
-        isRead: false,
+        subject: "Just a Quick Check-In",
+        body: "Hi there, I hope this email finds you well. I wanted to touch base and see how things are going on your end. Let me know if there's anything I can assist you with!",
+        isRead: true,
         isStarred: false,
-        sentAt: 1551133930594,
+        sentAt: 1646179200000, // March 1, 2022
         from: "autoMail3@gmail.com",
         to: "user@appsus.com",
       },
       {
         id: "e103",
-        subject: "Hi!",
-        body: "I am here too",
+        subject: "Saying Hi!",
+        body: "Hello! I hope you're having a fantastic day. Just wanted to drop a quick message to say hi and see how you're doing. Let's catch up soon!",
         isRead: false,
         isStarred: false,
-        sentAt: 1551133930594,
-        from: "user@appsus.com",
+        sentAt: 1646256000000, // March 2, 2022
+        from: "barrr@appsus.com",
         to: "autoMail3@gmail.com",
       },
+      {
+        id: "e104",
+        subject: "Breaking News: Your Weekly Digest",
+        body: "Dear Subscriber, Stay updated with our weekly digest featuring the latest news, trends, and insights from around the world. Click to read more!",
+        isRead: true,
+        isStarred: true,
+        sentAt: 1646030400000, // February 28, 2022
+        from: "news@weeklydigest.com",
+        to: "user@appsus.com",
+      },
+      {
+        id: "e105",
+        subject: "Invitation: Exclusive Event Invitation",
+        body: "You're invited to an exclusive event happening next week! Join us for an evening filled with networking opportunities, insightful discussions, and much more.",
+        isRead: false,
+        isStarred: true,
+        sentAt: 1646448000000, // March 4, 2022
+        from: "events@exclusiveinvite.com",
+        to: "user@appsus.com",
+      },
+      {
+        id: "e106",
+        subject: "Request for Feedback: Your Opinion Matters!",
+        body: "Dear valued customer, We're continuously striving to improve our services, and your feedback is invaluable to us. Please take a moment to share your thoughts and suggestions.",
+        isRead: true,
+        isStarred: false,
+        sentAt: 1646102400000, // February 28, 2022
+        from: "feedback@yourcompany.com",
+        to: "user@appsus.com",
+      },
+      {
+        id: "e107",
+        subject: "Reminder: Upcoming Webinar",
+        body: "Don't forget to mark your calendars for our upcoming webinar on [Topic]. Gain insights from industry experts and enhance your knowledge. Register now!",
+        isRead: false,
+        isStarred: false,
+        sentAt: 1646350800000, // March 3, 2022
+        from: "webinar@eventplanner.com",
+        to: "user@appsus.com",
+      },
+      {
+        id: "e108",
+        subject: "Discover Something New!",
+        body: "Hello! Explore our latest collection of products/services designed to enhance your lifestyle. Discover something new today!",
+        isRead: true,
+        isStarred: true,
+        sentAt: 1646091600000, // February 28, 2022
+        from: "discover@newcollection.com",
+        to: "user@appsus.com",
+      },
     ];
+
     utilService.saveToStorage(STORAGE_KEY, emails);
   }
 }
 
-function getDefaultFilter() {
-  return {
-    subject: "",
-  };
+function _filter(emails, filterBy) {
+  return emails.filter((email) => {
+    const text = filterBy.text.toLowerCase();
+    const subject = email.subject.toLowerCase();
+    const from = email.from.toLowerCase();
+    const to = email.to.toLowerCase();
+    const body = email.body.toLowerCase();
+    const isRead = filterBy.isRead;
+    const isStarred = filterBy.isStarred;
+
+    return (
+      (subject.includes(text) ||
+        body.includes(text) ||
+        from.includes(text) ||
+        to.includes(text)) &&
+      (isRead === "all" || email.isRead.toString() === isRead) &&
+      (isStarred === "all" || email.isStarred.toString() === isStarred)
+    );
+  });
 }
+// Check if the email matches the filter criteria

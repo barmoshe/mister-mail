86fb2bca389ad3e6d22e01cef461f7e26aabae4c
barmoshe <1barmoshe1@gmail.com>
2024-03-23 15:33:50 +0200

    mobile responsiveness of the header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,7 +1,5 @@
 import { NavLink } from "react-router-dom";
-
 import { toggleSidebar, filterByText } from "../services/event-bus.service";
-
 import logo from "../assets/imgs/MisterMail-logo.png";
 import menuSvg from "../assets/imgs/menu.svg";
 import support from "../assets/imgs/question_mark.svg";
@@ -20,38 +18,40 @@ export function AppHeader() {
   function handleMenuClick() {
     toggleSidebar();
   }
+
   function handleSearch(ev) {
     filterByText(ev.target.value);
   }
+
   return (
     <header className="app-header">
       <div className="menu-logo-container">
         <img
           src={menuSvg}
           alt="menu"
-          className="header-icon"
+          className="header-icon menu-icon"
           onClick={() => handleMenuClick()}
         />
         <NavLink to="/emails/inbox">
           <img src={logo} alt="logo" className="header-logo" />
         </NavLink>
       </div>
-      <div className="search-bar">
-        <input type="text" name="text" onChange={handleSearch} />
-      </div>
       <div className="header-right-side">
-        {icons.map((icon, index) => {
-          return (
+        <div className="search-bar">
+          <input
+            type="text"
+            name="text"
+            onChange={handleSearch}
+            placeholder="Search..."
+          />
+        </div>
+        <div className="icons-container">
+          {icons.map((icon, index) => (
             <NavLink to="/emails/charts" key={index}>
-              <img
-                src={icon.src}
-                alt={icon.alt}
-                key={index}
-                className="header-icon"
-              />
+              <img src={icon.src} alt={icon.alt} className="header-icon" />
             </NavLink>
-          );
-        })}
+          ))}
+        </div>
       </div>
     </header>
   );
